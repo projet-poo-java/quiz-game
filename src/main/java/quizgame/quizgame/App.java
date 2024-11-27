40419749256c742a9657a95011bd8f6e751d3622
aminbeno <aminebenhadare@gmail.com>
2024-11-27 12:43:18 +0100

    quiz page2

--- src/main/java/quizgame/quizgame/App.java
+++ src/main/java/quizgame/quizgame/App.java
@@ -10,14 +10,18 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("views/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/quizgame/quizgame/views/hello-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Quiz Game");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+        System.out.println("Hello World!");
     }
 }
+
+
--- src/main/java/quizgame/quizgame/controllers/HelloController.java
+++ src/main/java/quizgame/quizgame/controllers/HelloController.java
@@ -1,14 +1,33 @@
 package quizgame.quizgame.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
+
+    // Lier le Label welcomeText du fichier FXML
     @FXML
     private Label welcomeText;
 
+    // Méthode appelée lorsque l'utilisateur clique sur "Start Quiz"
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onStartQuizButtonClick() {
+        try {
+            // Charger le fichier quiz_page-view.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/quizgame/quizgame/views/quiz_page-view.fxml"));
+            Parent root = loader.load();
+
+            // Obtenir la scène actuelle et la remplacer
+            Stage stage = (Stage) welcomeText.getScene().getWindow();
+            stage.setScene(new Scene(root, 800, 600));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
